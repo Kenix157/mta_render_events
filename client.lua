d836dbe8f7c3246adfa6e5fb4c6db2c12fc43e1e
Ilya Volkov <kenix157@gmail.com>
2019-03-07 00:34:04 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,6 +1,13 @@
 --------------------
--- Author: IIYAMA --
+-- Author: IIYAMA (Updated by Kenix) --
 --------------------
+
+
+local addEventHandler 		= addEventHandler;
+local removeEventHandler 	= removeEventHandler;
+local table_remove			= table.remove;
+local unpack				= unpack;
+local len					= table.getn;
 
 local renderEvents = {
 	"onClientRender",
@@ -13,12 +20,9 @@ local allTargetFunctions = {} -- All attached functions will be stored in this t
 local acceptedRenderEventTypes = {} -- Is type in use? See: renderEvents.
 local renderEventTypeStatus = {} -- Is the event in use? (so it doesn't have to be attached again)
 
-
-
-
 do
 	-- prepare the data
-	for i=1, #renderEvents do
+	for i=1, len( renderEvents ) do
 		local event = renderEvents[i]
 		allTargetFunctions[event] = {}
 		acceptedRenderEventTypes[event] = true
@@ -29,7 +33,7 @@ end
 -- render all events here
 local processTargetFunction = function (timeSlice)
 	local targetFunctions = allTargetFunctions[eventName]
-	for i=#targetFunctions, 1, -1  do
+	for i=1, len( targetFunctions ) do
 		local targetFunctionData = targetFunctions[i]
 		local arguments = targetFunctionData[2]
 		if not arguments then
@@ -45,14 +49,13 @@ local processTargetFunction = function (timeSlice)
 end
 
 
-
 -- check if a function is already attached
 local checkIfFunctionIsTargetted = function (theFunction, event)
 	if not event or not acceptedRenderEventTypes[event] then
 		event = "onClientRender"
 	end
 	local targetFunctions = allTargetFunctions[event]
-	for i=1, #targetFunctions do
+	for i=1, len( targetFunctions ) do
 		if targetFunctions[i][1] == theFunction then
 			return true
 		end
@@ -67,7 +70,7 @@ function addRenderEvent(theFunction, event, ...)
 	end
 	if not checkIfFunctionIsTargetted(theFunction) then
 		local targetFunctions = allTargetFunctions[event]
-		targetFunctions[#targetFunctions + 1] = {theFunction, {...}}
+		targetFunctions[ len( targetFunctions ) + 1] = {theFunction, {...}}
 		
 		-- attach an event
 		if not renderEventTypeStatus[event] then
@@ -85,10 +88,10 @@ function removeRenderEvent(theFunction, event)
 		event = "onClientRender"
 	end
 	local targetFunctions = allTargetFunctions[event]
-	for i=1, #targetFunctions do
+	for i=1, len( targetFunctions ) do
 		if targetFunctions[i][1] == theFunction then
-			table.remove(targetFunctions, i)
-			if #targetFunctions == 0 then
+			table_remove(targetFunctions, i)
+			if len( targetFunctions ) == 0 then
 				if renderEventTypeStatus[event] then
 					removeEventHandler (event, root, processTargetFunction)
 					renderEventTypeStatus[event] = false
